2be344d13fb674e101ef823c9ceb41f18b9c1e9d
Aaron Hedges <aaron@dashron.com>
2021-10-30 15:30:15 -0400

    get the fade working

--- src/index.ts
+++ src/index.ts
@@ -23,29 +23,95 @@ app.get("/", (req, res) => {
 	});
 });
 
-// Make all images on the page invisible, then show the first image full screen and cycle through the images every 5 seconds
 const scriptContents = `
+	// Ensure we sort 10 after 9, and not after 1.
+	imgSrcs.sort((a,b) => (a).localeCompare(b, undefined, {numeric: true}));
+
 	let currentImage = 0;
 
+	// Function to preload the next image
 	function loadNext() {
 		let next = new Image();
 		currentImage += 1;
 		next.src = imgSrcs[currentImage];
 	}
 
-    let image = document.getElementById('image');
+	// Load the first image into the background image
+    let image = document.getElementById('imageback');
 	image.src = imgSrcs[currentImage];
 
-	// This ensures it takes 8 hours no matter how many images exist
-	// const interval = ((8 * 60 * 60) / imgSrcs.length) * 1000;
-	const interval = 1000;
+	// Preload the second image
+	loadNext();
 
-	setInterval(() => {
-		let image = document.getElementById('image');
-		image.src = imgSrcs[currentImage];
+	function cycle() {
+		let imageback = document.getElementById('imageback');
+
+		// Load the foreground image
+		let imagefront = new Image();
+		imagefront.id = 'imagefront';
+		imagefront.classList.add('fadein');
+		imagefront.src = imgSrcs[currentImage];
+
+		// When the foreground images animation is done, move it to the background, remove the foreground image and repeat the process
+		imagefront.addEventListener('animationend', () => {
+			imageback.src = imagefront.src;
+			imagefront.remove();
+			cycle();
+		});
+
+		// Add the foreground image to the page
+		imageback.parentNode.insertBefore(imagefront, imageback.nextSibling);
+
+		// Preload the next image
 		loadNext();
-	}, interval);
-	console.log('interval', ((8 * 60 * 60) / imgSrcs.length) * 1000);
+	}
+
+	cycle();
+`;
+
+const styleContents = `
+	img {
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	#imagefront.fadein {
+		--fadeInTime: 2s;
+
+		animation: fadeIn var(--fadeInTime);
+		-webkit-animation: fadeIn var(--fadeInTime);
+		-moz-animation: fadeIn var(--fadeInTime);
+		-o-animation: fadeIn var(--fadeInTime);
+		-ms-animation: fadeIn var(--fadeInTime);
+		display: block;
+		z-index 5;
+	}
+
+	@keyframes fadeIn {
+		0% {opacity:0;}
+		100% {opacity:1;}
+	}
+
+	@-moz-keyframes fadeIn {
+		0% {opacity:0;}
+		100% {opacity:1;}
+	}
+
+	@-webkit-keyframes fadeIn {
+		0% {opacity:0;}
+		100% {opacity:1;}
+	}
+
+	@-o-keyframes fadeIn {
+		0% {opacity:0;}
+		100% {opacity:1;}
+	}
+
+	@-ms-keyframes fadeIn {
+		0% {opacity:0;}
+		100% {opacity:1;}
+	}
 `;
 
 /*
@@ -64,11 +130,14 @@ app.get("/:folder", (req, res) => {
 			}
 
 			let response = `
-			<img src="" id="image" />
+			<img src="" id="imageback" />
 			<script>
 				var imgSrcs = [${links.join(', ')}];
 				${scriptContents}
-			</script>`
+			</script>
+			<style>
+				${styleContents}
+			</style>`
 
 			// send the list of links
 			res.send(response);
